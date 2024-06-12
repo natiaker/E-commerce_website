f6f78d4d686ba7914100ac3d0367dd0ad4c22c2a
natiaker <n.kerdzaia@gmail.com>
2024-06-12 18:15:40 +0400

    add navbar, index and detail pages

--- ecommerce/shop/urls.py
+++ ecommerce/shop/urls.py
@@ -6,5 +6,5 @@ app_name = 'shop'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='homepage'),
-
+    path('product_details/<int:pk>', views.ProductDetailsView.as_view(), name='product_details'),
 ]
--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -1,6 +1,30 @@
+from django.db.models import Q
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
+from .models import Product, Category
+
+
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        query = request.GET.get('product_query')
+
+        if query:
+            products = Product.objects.filter(Q(title__icontains=query))
+        else:
+            products = Product.objects.all()
+
+        return render(request, self.template_name, {'products': products})
+
+
+class ProductDetailsView(DetailView):
+    model = Product
+    template_name = 'product_details.html'
+    context_object_name = 'product_details'
+
+
+
